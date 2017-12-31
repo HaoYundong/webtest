242b8c13ef5c2be534ebfa9b5e4d7de38284c7d4
haoyundong <277079581@qq.com>
2017-12-31 18:50:50 +0800

    test

--- test/js/Script.js
+++ test/js/Script.js
@@ -2,6 +2,44 @@
  * Created by haoyundong on 2017/7/12.
  */
 
-function test() {
+function getUser() {
+    var url = "http://localhost:7000/user/users";
 
+    $.ajax({
+        type : "get",
+        async:false,
+        url : url,
+        dataType : "jsonp",
+        jsonp: "callback",//服务端用于接收callback调用的function名的参数
+        success : function(response){
+            adaptToUI(response['data']['users'])
+        },
+        error:function(){
+            alert('fail');
+        }
+    });
+}
+
+function adaptToUI(users) {
+    var tbody = document.getElementById("userData");
+
+    tbody.innerHTML = "";
+
+    for(var i = 0; i < users.length; i++) {
+        var tr = document.createElement("tr");
+
+        var td1 = document.createElement("td");
+        var td2 = document.createElement("td");
+        var td3 = document.createElement("td");
+
+        td1.innerHTML = users[i]['id'];
+        td2.innerHTML = users[i]['name'];
+        td3.innerHTML = users[i]['mobile'];
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        tbody.appendChild(tr);
+    }
 }
